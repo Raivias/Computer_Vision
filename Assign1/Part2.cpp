26aa028b1612eaad2c6c6b581f7fae0600d40a1f
raivias <raivias@gmail.com>
2016-01-30 16:31:11 -0500

    Started Part2.cpp loop should be all set up. Made cosmetic changes to Part1.cpp

---
+++ Assign1/Part2.cpp
@@ -0,0 +1,49 @@
+/** Part1.cpp
+	Computer Vision
+	Assignment 1 Part 2
+	Xavier Guay
+	PUROPSE:
+	Open a given image binary. Reduce it's size by half in both length and width
+**/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+
+
+
+/*This program takes one input:
+	file - 	A binary image file
+	Length- How tall the file is in pixels
+	Width- 	How wide the file is in pixels
+*/
+int main(int argv, char** argc){
+	//get inputs
+	//char string = argc[1];
+	int length = atoi(argc[2]);
+	int width = atoi(argc[3]);
+	
+	printf("running\n");
+
+	//open file to copy from
+	//open new file to edit
+
+	//loop through the file copying each pixel if nessary
+	for(int lCount = 0; lCount <= length; lCount += 2){
+		printf("%d: ", lCount);
+		/*loop through line of image. because we want to copy the left half
+		we only copy the first half of the line*/
+		for(int wCount = 0; wCount <= width/2; wCount += 2){
+			//copy pixel at wCount
+			printf("%d ", wCount);
+		}
+		printf("\n");
+	}
+	
+	//close files	
+
+	//display new image
+
+	printf("ran\n");	
+	return 1;
+}
